d1234504a01751a48a31319a356fb19392caa827
Pav17 <148437857+pavanth17@users.noreply.github.com>
2026-07-23 13:22:07 +0530

    Time: 32 ms (10.17%) | Memory: 34.2 MB (16.95%) - LeetSync

---
+++ 3824-number-of-unique-xor-triplets-i/number-of-unique-xor-triplets-i.py
@@ -0,0 +1,9 @@
+class Solution:
+    def uniqueXorTriplets(self, nums: List[int]) -> int:
+        n=len(nums)
+        if(n<=2):
+            return n
+        mask=0
+        for num in nums:
+            mask|=num
+        return mask+1
